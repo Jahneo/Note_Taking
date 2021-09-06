5ae41076b9f81a0916b44e4c80f83f84ee7e96b4
Jahneo <85087102+Jahneo@users.noreply.github.com>
2021-09-05 21:37:34 -0400

    setting environment for Heroku

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
+const path = require('path');
 const {db} = require('./Develop/db/db');
 const express = require('express');
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.listen(3000, () => {
     console.log(`API server now on port 3000!`);
@@ -9,6 +12,16 @@ app.listen(3000, () => {
     const result = notesArray.filter(db => db.id === id)[0];
     return result;
   }
+
+  function createNewNotes(body, notesArray){
+      const db = body;
+      notesArray.push(db);
+      fs.writeFileSync(
+          path.join(__dirname,'./Develop/db/db.json'),
+          JSON.stringify({db:notesArray}, null,2)
+      );
+      return db;
+  }
   // function to get search results and return error if no results 
   app.get('/api/db/:id',(req, res) => {
       const result = findById(req.params.id, db);
@@ -17,4 +30,11 @@ app.listen(3000, () => {
       } else {
         res.send(404);
       }
+  });
+  app.post('/api/db/',(req, res) => {
+    //allow id's to be unique keys so increases after the last entry
+      req.body.id = db.length.toString();
+      //call on function to add new notes
+      const db = createNewNotes (req.body, db);
+      res.json(db);
   });
