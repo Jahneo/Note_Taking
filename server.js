19ef2a9193675dc5a1917961bd153574b2cc7dac
Jahneo <85087102+Jahneo@users.noreply.github.com>
2021-09-07 12:26:30 -0400

    add get  and post functions and initilas heroku to point to https://note-taking-challenge.herokuapp.com

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+//const cool = require('cool-ascii-faces');
 const fs = require('fs');
 const path = require("path");
-const newNotes = require('./Develop/db/db.json');
+const {db} = require('./Develop/db/db.json');
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -19,12 +20,30 @@ app.use(express.static('public'));
   app.get('/notes',(req, res) => {
       res.sendFile(path.join(__dirname,'.Develop/public/notes.html'));
   });
-  // function to search notes 
+
+  app.route('/api/notes')
+      //.get('/api/notes',(req, res) => {
+     // res.json({db});
+     // })
+/*
+      .post('/api/notes',(req, res) => {
+        res.sendFile(path.join(__dirname,'.Develop/public/notes.html'));
+        let incomingNotes = req.body;
+        let highestPossibleNotes = 80;
+        for (let i = 0; i < db.length; i++) {
+          let addNotes = db[i];
+          if (addNotes.id > incomingNotes)
+          addNotes =  incomingNotes.id;
+        }
+      })
+      incomingNotes.id = highestPossibleNotes + 1;
+*/
+ //function to search notes 
   function findById(id, notesArray) {
     const result = notesArray.filter(db => db.id === id)[0];
     return result;
   }
-// function to create notes
+ //function to create notes
   function createNewNotes(body, notesArray){
       const db = body;
       notesArray.push(db);
@@ -34,7 +53,7 @@ app.use(express.static('public'));
       );
       return db;
   }
-  
+
   // function to get search results and return error if no results 
   app.get('/api/db/:id',(req, res) => {
       const result = findById(req.params.id, db);
@@ -51,6 +70,7 @@ app.use(express.static('public'));
       const db = createNewNotes (req.body, db);
       res.json(db);
   });
+  
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
