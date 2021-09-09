7b72ad79cd38a2141c0e17308628f16ac960ecdf
Jahneo <85087102+Jahneo@users.noreply.github.com>
2021-09-09 11:08:03 -0400

    restarting adding Heroku

--- server.js
+++ server.js
@@ -16,6 +16,19 @@ app.get('/api/db',(req,res)=> {
   }
     res.json(results);
 });
+app.get('/api/db/:id',(req,res)=> {
+  const result = findById(req.params.id, db);
+  if (result) {
+  res.json(result);
+  }else {
+    res.status(404).send('Sorry can not find that page');
+  }
+});
+function findById (id, dbArray) {
+  const result = dbArray.filter(db => db.id == id)[0];
+  return result;
+}
+
 function filterByQuery(query, dbArray) {
   let filteredResults = dbArray;
   if (query.id) {
@@ -28,90 +41,6 @@ function filterByQuery(query, dbArray) {
   return filteredResults;
 }
 
-/*
-  //returns index page 
-  app.get('/',(req, res) => {
-        res.send('Welcome to My Note Taking Challenge');
-        res.sendFile(path.join(__dirname,'./Develop/public/index.html'))
-    
-});
-//returns the notes.html page
-  app.get('/notes',(req, res) => {
-      res.sendFile(path.join(__dirname,'./Develop/public/notes.html'));
-  });
-
-  app.route('/api/notes')
-      //.get('/api/notes',(req, res) => {
-     // res.json({db});
-     // })
-
-
-      app.post('/api/notes',(req, res) => {
-        res.sendFile(path.join(__dirname,'.Develop/public/notes.html'));
-        let incomingNotes = req.body;
-        let highestPossibleNotes = 80;
-        for (let i = 0; i < db.length; i++) {
-          let addNotes = db[i];
-          if (addNotes.id > incomingNotes) {
-          addNotes =  incomingNotes.id;
-        }
-      }
-      
-      incomingNotes.id = highestPossibleNotes + 1;
-      db.push(addNotes)
-
-      fs.writeFile(jsonFilePath, JSON.stringify(db), function(err){
-        if(err) {
-          return console.log(err);
-        }
-        console.log("New entry was accepted")
-      });
-      res.json(addNotes);
-    });
-*/
-/*
- //function to search notes 
-  function findById(id, notesArray) {
-    const result = notesArray.filter(db => db.id === id)[0];
-    return result;
-  }
- //function to create notes
-  function createNewNotes(body, notesArray){
-      const db = body;
-      notesArray.push(db);
-      fs.writeFileSync(
-          path.join(__dirname,'./Develop/db/db.json'),
-          JSON.stringify({db:notesArray}, null,2)
-      );
-      return db;
-  }
-
-  // function to get search results and return error if no results 
-  app.get('/api/db/id',(req, res) => {
-      const result = findById(req.params.id, db);
-      if (result) {
-        res.json(db);
-      } else {
-       res.send(404);
-      }
-  });
- 
-  app.get('/api/db/',(req, res) => {
-     const result = findById(req.params.id, db);
-     if (result) {
-       res.json(db);
-     } else {
-       res.send(404);
-     }
- });
-  app.post('/api/db',(req, res) => {
-    //allow id's to be unique keys so increases after the last entry
-      req.body.id = db.length.toString();
-      //call on function to add new notes
-      const db = createNewNotes (req.body, db);
-      res.json(db);
-  });
-  */
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
