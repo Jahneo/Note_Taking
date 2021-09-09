fb85205d8f615b81de4fcb5bcd5bea93806522b7
Jahneo <85087102+Jahneo@users.noreply.github.com>
2021-09-09 12:20:27 -0400

    add the get and post function install and use imsomnia

--- server.js
+++ server.js
@@ -20,25 +20,43 @@ app.get('/api/db/:id',(req,res)=> {
   const result = findById(req.params.id, db);
   if (result) {
   res.json(result);
-  }else {
+    }else {
     res.status(404).send('Sorry can not find that page');
   }
 });
-function findById (id, dbArray) {
-  const result = dbArray.filter(db => db.id == id)[0];
+app.post('/api/db',(req, res) =>{
+   //incoming post store in req.body
+  req.body.id = db.length.toString();
+  const notes = createNewNotes(req.body, db);
+   res.json(req.body);
+});
+
+
+function findById (id, notesArray) {
+  const result = notesArray.filter(db => db.id == id)[0];
   return result;
 }
 
-function filterByQuery(query, dbArray) {
-  let filteredResults = dbArray;
+function filterByQuery(query, notesArray) {
+  let filteredResults = notesArray;
   if (query.id) {
     filteredResults = filteredResults.filter(db => db.id === query.id);
   }
   if (query.title) {
-    filteredResults = filteredResults.filter(db => db.title === query.title);
+    filteredResults = filteredResults.filter(db => db.Title === query.Title);
   }
   
   return filteredResults;
+}
+function createNewNotes (body, notesArray) {
+  const db= body;
+  notesArray.push(db);
+  fs.writeFileSync(
+    path.join(__dirname, './Develop/db/db.json'),
+    JSON.stringify({ db: notesArray }, null, 2)
+  );
+  //console.log(body);
+  return db;
 }
 
   app.listen(PORT, () => {
